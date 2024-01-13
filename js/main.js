d1e0d1edcfb874636a59d3bcd414b67f69eb1769
flanderos <andershellerud@hotmail.no>
2024-01-13 17:56:12 +0100

    added redirect function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 import { registerUser } from "./auth/register.js";
 import { loginUser } from "./auth/login.js";
 import { validateForm } from "./auth/validation.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const token = localStorage.getItem("token");
+    if (token) {
+        window.location.href = "login.html"; //redirect user to login.html if localstorage contains token
+    } else {
+        window.location.href = "register.html"; 
+    }
+});
