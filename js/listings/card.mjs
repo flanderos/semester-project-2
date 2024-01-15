2304b102983a4a588c624200c461e5b038942204
flanderos <andershellerud@hotmail.no>
2024-01-15 20:03:12 +0100

    styling

--- js/listings/card.mjs
+++ js/listings/card.mjs
@@ -17,21 +17,25 @@ export const renderListings = async () => {
         const media = results[i].media[0] ? results[i].media[0] : "../../assets/placeholder.png";
         
         listings.innerHTML += `
-        <div class="h-96 w-96 border border-black rounded p-5 mt-5 bg-white shadow-lg flex flex-col justify-between">
+        <div class="w-96 h-96 border border-black rounded p-5 mt-5 bg-white shadow-lg flex flex-col justify-between">
             <div class="flex flex-row justify-between mb-5">
                 <p class="text-xl" id="title">${title}</p>
-                <p class="text-xl" id="timeCreated">Created:${created}</p>
+                <p class="text-xl" id="timeCreated">Created: ${created}</p>
             </div>
-            <p class="text-lg" id="tags">${tags}</p>
-            <img class="h-72 w-72" src="${media}"onerror="this.onerror=null; this.src='../../assets/placeholder.png';" alt="placeholder image" />
-            <p class="mt-5">Current Bid:</p>
-            <p>Time Left:</p>
-            <div class="flex justify-between mt-5">
+            <div class="flex-grow">
+                <img class="w-full h-full object-cover" src="${media}" onerror="this.onerror=null; this.src='../../assets/placeholder.png';" alt="placeholder image" />
+            </div>
+            <div>
+                <p class="text-lg" id="tags">${tags}</p>
+                <p class="mt-1">Current Bid:</p>
+                <p>Time Left:</p>
+            </div>
+            <div class="flex justify-between mt-2">
                 <div class="flex flex-row gap-2">
-                    <button class="w-36 bg-customBlue rounded shadow-lg hover:underline font-inder">BID</button>
-                    <input type="number" class="rounded w-28 shadow-lg border" />
-                </div>
-                <button class="w-36 bg-customBlue rounded shadow-lg hover:underline font-inder">Buy Now</button>
+                <button class="w-36 bg-customBlue rounded shadow-lg hover:underline font-inder">BID</button>
+                <input type="number" class="rounded w-28 shadow-lg border" />
+            </div>
+            <button class="w-36 bg-customBlue rounded shadow-lg hover:underline font-inder">Buy Now</button>
             </div>
         </div>`;
 }
