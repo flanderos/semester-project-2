6dd2bba0347c1890c5d9b2748291fc534761ec90
flanderos <andershellerud@hotmail.no>
2024-01-20 22:00:07 +0100

    profilecard now renders with javascript

--- js/profile.mjs
+++ js/profile.mjs
@@ -1,2 +1,3 @@
 import { getListingsByProfile } from "./scripts/getlistingbyprofile.mjs";
 import { logOut } from "./scripts/logout.mjs";
+import { renderProfileCard } from "./profile/localstorageprofiledata.mjs";
