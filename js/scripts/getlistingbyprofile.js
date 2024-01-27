c240ba19a3a3094846895c836a6da8a0544a2b11
flanderos <andershellerud@hotmail.no>
2024-01-27 14:21:36 +0100

    user is now redirected to auctionsite and get a message that says the post is successfully posted

--- js/scripts/getlistingbyprofile.js
+++ js/scripts/getlistingbyprofile.js
@@ -1,5 +1,5 @@
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjE3NSwibmFtZSI6IkFuZGVyczgiLCJlbWFpbCI6ImFuZGVyczhAbm9yb2ZmLm5vIiwiYXZhdGFyIjoiIiwiY3JlZGl0cyI6MTAwMCwid2lucyI6W10sImlhdCI6MTcwNTQzMzMwNn0.BXT1x1AwIiWOfF7g4MMrFsEXlJt_mIiDrl0a6qUeXEw";
-const userName = "Espen";
+const userName = "Anders8";
 
 export const getListingsByProfile = async () => {
     const url = `https://api.noroff.dev/api/v1/auction/profiles/${userName}/listings`;
