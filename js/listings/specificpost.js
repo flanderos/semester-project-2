b5c8fe9f179a222a994f032a789802cf58a8466f
flanderos <andershellerud@hotmail.no>
2024-01-28 15:04:25 +0100

    fetching more info on the specific post

--- js/listings/specificpost.js
+++ js/listings/specificpost.js
@@ -13,7 +13,7 @@ export function getSpecificPost() {
     const thePost = document.querySelector("#thepost");
 
     // Fetch api for the specific post
-    fetch(`https://api.noroff.dev/api/v1/auction/listings/${postId}`)
+    fetch(`https://api.noroff.dev/api/v1/auction/listings/${postId}?_seller&_bids=true`)
         .then(response => {
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
@@ -22,24 +22,43 @@ export function getSpecificPost() {
         })
         .then(data => {
             // render data
-            const { created, title, media, tags, endsAt } = data; // Bruk data fra API-forespørselen
+            const { created, title, media, tags, endsAt, description, bids } = data; // Bruk data fra API-forespørselen
             const mediaUrl = media[0] ? media[0] : "../../assets/placeholder.png";
             const timeLeft = getTimeLeft(endsAt);
             const timeLeftString = formatTimeLeft(timeLeft);
-
-            thePost.innerHTML = `<div class="h-2/3 w-[80vw] mt-8 flex justify-center items-center" data-id=${postId} id="thepost">
+            
+            const bidsList = document.createElement('ul');
+            
+            bids.forEach((bid) => {
+                const bidItem = document.createElement('li');
+                bidItem.textContent = `Amount: ${bid.amount}, Name: ${bid.bidderName}`;
+                bidsList.appendChild(bidItem);
+            });
+            
+            // Finn siste bud
+            const lastBid = bids[bids.length - 1];
+            
+            console.log(data);
+            
+            thePost.innerHTML = `<div class="h-2/3 w-[80vw] mt-8 flex justify-center items-center mb-12" data-id=${postId} id="thepost">
                 <div class="w-[70%] h-[70%] border border-black rounded p-5 bg-white shadow-lg flex flex-col justify-between max-w-[calc(100%/2)]">
                     <div class="flex flex-col justify-between mb-5">
                         <p class="text-lg" id="timeCreated">Created: ${created}</p>
-                        <p class="font-bold" id="title">${title}</p>
+                        <p class=" text-lg font-bold" id="title">${title}</p>
+                        <p class="text-sm">${description}</p>
                     </div>
                     <div class="flex-grow">
                         <img class="w-11/12 h-2/3 object-cover" src="${mediaUrl}" onerror="this.onerror=null; this.src='../../assets/placeholder.png';" alt="placeholder image" />
                     </div>
                     <div>
-                        <p class="text-s" id="tags">${tags || ""}</p>
-                        <p class="mt-1" text-s>Current Bid:</p>
-                        <p>Time Left: <span id="time-left">${timeLeftString}</span></p>
+                        <p class="text-s" id="tags">${tags || "tags"}</p>
+                        <p class="mt-1" text-s>Bids:</p>
+                        <ul class="list-inside list-disc pl-4 text-sm text-green-700">
+                            ${bidsList.outerHTML}
+                        </ul>
+                        <p class="font-bold">Current Bid: ${lastBid ? lastBid.amount + " " + "Coins" : "N/A" }</p>
+                        <p class="">Time Left: <span id="time-left">${timeLeftString}</span></p>
+                        
                     </div>
                     <div class="flex justify-start mt-2">
                         <button class="w-36 bg-customBlue rounded shadow-lg hover:underline font-inder">BID</button>
@@ -47,6 +66,8 @@ export function getSpecificPost() {
                     </div>
                 </div>
             </div>`;
+            ;
+            
 
             const timeLeftElement = document.getElementById('time-left');
             setInterval(() => {
