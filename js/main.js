de66799eb65eac70e1c15fad3c34fc6b48663220
flanderos <andershellerud@hotmail.no>
2024-02-03 14:33:09 +0100

    added function to set userloginstatus

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
 import { registerUser } from "./auth/register.js";
 import { loginUser } from "./auth/login.js";
 import { validateForm } from "./auth/validation.js";
+import { userIsLoggedIn } from "./scripts/setuserloginstatus.js";
+import { userIsLoggedOut } from "./scripts/setuserloginstatus.js";
